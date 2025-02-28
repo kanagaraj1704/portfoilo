99360a5b809fbce973013d2b2d01810d6eecb510
kanagaraj <kanagaraj@nanonino.in>
2025-02-28 15:56:58 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { LoginPage } from "./page/login"
+
+function App() {
+
+  return (
+    <div>
+      <LoginPage />
+    </div>
+  )
+}
+
+export default App
---
+++ src/page/login.tsx
@@ -0,0 +1,8 @@
+export const LoginPage = () => {
+    return (
+        <div style={{ justifyContent: 'center', textAlign: 'center' }}>
+            <div>This screen for the login</div>
+            <img style={{ height: '220px', width: '220px' }} src="https://img.freepik.com/free-vector/oops-404-error-with-broken-robot-concept-illustration_114360-5529.jpg" alt="404" />
+        </div>
+    )
+}
